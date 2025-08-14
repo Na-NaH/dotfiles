305a9a284e8240fa311c20759b4817f0ec8b8b2f
Na-NaH <cristi4npedroz0@gmail.com>
2025-08-14 18:33:23 -0300

    Script for copy the files and restore back to system.

--- manager.sh
+++ manager.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Permissions: chmod +x copy.sh
+# Permissions: chmod +x manager.sh
 
 # Path to the git directory
 DOTFILES="$HOME/Casa/Workbench/Giteando/personal/Dotfiles"
+
 # Create the directory if not exist
 mkdir -p "$DOTFILES"
 
@@ -24,25 +25,25 @@ CONFIGS=(
   ".config/MangoHud"
 )
 
-read -p "[ 1 ] Create a symlink. [ 2 ] Copy to system. [ * ] Skip: " CHOICE
+read -p "[ 1 ] Copy to dotfiles. [ 2 ] Restore to system. [ * ] Skip: " CHOICE
 
 case "$CHOICE" in
   1) 
-  # Creating backup
+  # Copy to dotfiles
   for item in "${CONFIGS[@]}"; do
     item_dir=$(dirname "$item") # Get the path
     mkdir -p "$DOTFILES/$item_dir" # Create the directory if needed
       if [ -e "$HOME/$item" ]; then
         echo "Copying $item..."
-        ln -s -f "$HOME/$item" "$DOTFILES/$item_dir/"
+        cp -r "$HOME/$item" "$DOTFILES/$item_dir/"
       else
         echo "Warning: $HOME/$item does not exist, skipping..."
       fi
   done
-  echo "Creation completed. Symlink from $HOME to $DOTFILES"
+  echo "Creation completed. From $HOME to $DOTFILES"
     ;;
   2)
-  # Restoring to the system
+  # Restoring to system
   for item in "${CONFIGS[@]}"; do
     item_dir=$(dirname "$item") # Get the path
     mkdir -p "$HOME/$item_dir" # Create the directory if needed
@@ -53,7 +54,7 @@ case "$CHOICE" in
         echo "Warning: $RELATIVE/$item does not exist, skipping..."
       fi
   done
-  echo "Restore operation completed. Files copied from $RELATIVE to $HOME"
+  echo "Restore operation completed. From $RELATIVE to $HOME"
     ;;
   *)
     echo "Skip or Invalid choice."
